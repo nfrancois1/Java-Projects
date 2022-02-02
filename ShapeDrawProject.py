854cfa211c9896bc29c89ce515da1b865ece6e42
nfrancois1 <91170751+nfrancois1@users.noreply.github.com>
2022-02-01 20:16:49 -0500

    Add files via upload

---
+++ ShapeDrawProject.py
@@ -0,0 +1,45 @@
+"""
+Noah Francois
+Shape Drawing Project (Python):
+    - Three objects possible to draw. Line, Triangle, or Sqaure. User 
+      determines width of object.
+"""
+
+
+import turtle
+t = turtle.Turtle()
+
+c = input("Enter Color: ")
+w = int(input("Enter Width: "))
+l = int(input("Enter Length: "))
+s = input("Enter shape(line, triangle or square) : ")
+
+if(s == "line"):
+    t.color(c)
+    t.width(w)
+    t.forward(l)
+
+elif(s == "triangle"):
+    t.color(c)
+    t.width(w)
+    t.forward(l)
+    t.left(120)
+    t.forward(l)
+    t.left(120)
+    t.forward(l)
+
+elif(s == "square"):
+    t.color(c)
+    t.width(w)
+    t.forward(l)
+    t.left(90)
+    t.forward(l)
+    t.left(90)
+    t.forward(l)
+    t.left(90)
+    t.forward(l)
+
+else:
+    print("Invalid Input Detected")
+
+
